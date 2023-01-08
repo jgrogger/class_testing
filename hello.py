e2537672e97945d0d368b2bd7342b4deb5e98d8d
Jeffrey Grogger <82467170+jgrogger@users.noreply.github.com>
2023-01-08 15:28:56 -0600

    Create hello.py
    created class example

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 14:36:30 2023
+
+@author: jgrogger
+"""
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
+
+x=10
+print(2*x)
